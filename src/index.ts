06b9917e72d714c8d23d8092143125e68660d897
dyyyl <hey@dyyyl.rocks>
2020-01-15 15:31:57 +0100

    refactor to proper initial cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+import commander from 'commander';
+
+import sum from './sum';
+
+const program = new commander.Command();
+
+program
+  .version('0.1.0')
+  .description(`Mock  data objects shaped with your TypeScript interfaces`)
+  .option('-s, --say [string]', 'echos back string', 'hello, world!')
+  .requiredOption('-d, --double <number>', 'must choose a number to double')
+  .parse(process.argv);
+
+if (program.double) console.log(`sum: ${sum(program.double, program.double)}`);
+if (program.say) console.log(program.say);
