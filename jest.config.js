ae596fa312d4db7bbda12b4e42d104e628ac5a1b
dyyyl <hey@dyyyl.rocks>
2020-01-16 07:24:07 +0100

    moves config to individual file

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': './node_modules/ts-jest',
+  },
+  testEnvironment: 'node',
+};
