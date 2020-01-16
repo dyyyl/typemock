429947d25e14f92af571b1f933c16aad25017c2f
dyyyl <hey@dyyyl.rocks>
2020-01-16 07:25:32 +0100

    adds some general todos

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import commander from 'commander';
 
-import sum from './sum';
-
 const program = new commander.Command();
 
 program
+  .name('typemock')
   .version('0.1.0')
-  .description(`Mock  data objects shaped with your TypeScript interfaces`)
-  .option('-s, --say [string]', 'echos back string', 'hello, world!')
-  .requiredOption('-d, --double <number>', 'must choose a number to double')
+  .description(`Mock data objects shaped with your TypeScript interfaces`)
+  .option('-o, --out-dir [outDir]', 'echos back string', '.')
+  .requiredOption(
+    '-p, --path [path]',
+    'must choose a path to the interface intended for mocking',
+  )
+  .requiredOption(
+    '-i, --interfaces [interfaces]',
+    'must name an interface intended for mocking',
+  )
   .parse(process.argv);
 
-if (program.double) console.log(`sum: ${sum(program.double, program.double)}`);
-if (program.say) console.log(program.say);
+const { interfaces, outDir, path } = program;
+console.log({ outDir, path, interfaces });
+// TODO
+// 1. Access file
+// 2. Yeet interfaces by name & type
+// 3. forEach named interface, generate data object
+// 4. Write data object to file specified in outDir
